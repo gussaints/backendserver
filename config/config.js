025570d5f9082ba5ab8bafd83d68ad5765700a97
Gustavo Contreras <qwertygussaints@gmail.com>
2018-08-06 18:27:59 -0600

    Seccion 12 terminada - backend

--- config/config.js
+++ config/config.js
@@ -1,1 +1,3 @@
 module.exports.SEED = '1Aleluya';
+// Google
+module.exports.CLIENT_ID = '98934665845-1sfsuav19ojancmhbv833riln54jnj4e.apps.googleusercontent.com';
--- routes/login.route.js
+++ routes/login.route.js
@@ -5,6 +5,104 @@ var Usuario = require('../models/usuario.model');
 var jwt = require( 'jsonwebtoken' );
 var SEED = require('../config/config').SEED;
 
+// ==================================================
+// Google Auth
+// ==================================================
+var CLIENT_ID = require('../config/config').CLIENT_ID;
+const { OAuth2Client } = require( 'google-auth-library' );
+const client = new OAuth2Client( CLIENT_ID );
+// ==================================================
+// Google Auth
+// ==================================================
+async function verify( token ) {
+  const ticket = await client.verifyIdToken({
+      idToken: token,
+      audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+      // Or, if multiple clients access the backend:
+      //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+  });
+  const payload = ticket.getPayload();
+  const userid = payload['sub'];
+  // If request specified a G Suite domain:
+  //const domain = payload['hd'];
+  return {
+      nombre: payload.name,
+      email: payload.email,
+      img: payload.picture,
+      google: true
+  }
+}
+// ==================================================
+// Autenticacion de Google
+// ==================================================
+router.post( '/google', async( req, res, next ) => {
+
+    var token = req.body.token;
+
+    var googleUser = await verify( token )
+        .catch( e => {
+            console.log( e );
+            
+            return res.status(403).json({
+                ok: false,
+                message: 'token no valido'
+            });
+        })
+    
+    Usuario.findOne( { email: googleUser.email }, ( err, usuarioDB ) => {
+        console.log();
+        if ( err ) {
+            return res.status(500).json({
+                ok: false,
+                mensaje: 'Error al buscar usuario',
+                errors: err
+            });
+        } else if ( usuarioDB ) {
+            if ( usuarioDB.google === false ) {
+                // usuario se registro normalmente
+                return res.status(400).json({
+                    ok: false,
+                    mensaje: 'Debe usar su autenticacion normal'
+                });
+            } else {
+                // Crear un token !
+                // Usuario ya registrado mediante google
+                usuarioDB.password = ':-)';
+                var token = jwt.sign({ usuario: usuarioDB }, SEED, { expiresIn: 14400 }); // 4 horas == 14400
+                return res.status(200).json({
+                    ok: true,
+                    mensaje: 'Login POST correcto',
+                    token: token,
+                    usuario: usuarioDB,
+                    id: usuarioDB._id
+                });
+            }
+        } else {
+            // El usuario no existe, hay que crearlo
+            var usuario = new Usuario( );
+            usuario.nombre = googleUser.nombre;
+            usuario.email = googleUser.email;
+            usuario.img = googleUser.img;
+            usuario.google = true;
+            usuario.password = ':-)';
+
+            usuario.save( ( err, usuarioDB ) => {
+                var token = jwt.sign({ usuario: usuarioDB }, SEED, { expiresIn: 14400 }); // 4 horas == 14400
+                return res.status(200).json({
+                    ok: true,
+                    mensaje: 'Login POST correcto',
+                    token: token,
+                    usuario: usuarioDB,
+                    id: usuarioDB._id
+                });
+            })
+        }
+    })
+    
+});
+// ==================================================
+// Autenticacion normal
+// ==================================================
 router.post( '/', ( req, res, next ) => {
     var body = req.body;
 
